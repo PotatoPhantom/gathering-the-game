1b3055cec7cea5a80b374fd46e673d3f616125d7
PotatoPhantom <54708020+PotatoPhantom@users.noreply.github.com>
2022-06-15 01:02:46 -0700

    menu

--- Gathering/Assets/Scripts/MainMenu.cs
+++ Gathering/Assets/Scripts/MainMenu.cs
@@ -4,24 +4,26 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Canvas canvas;
     public GameObject connectPanel;
 
     public static string address;
     public static string port;
 
+    private void Awake()
+    {
+        connectPanel.SetActive(false);
+    }
+
     public void Host()
     {
         NetworkManager.Singleton.StartHost();
+        canvas.gameObject.SetActive(false);
     }
 
     public void Join()
     {
         NetworkManager.Singleton.StartClient();
-    }
-
-    public void HidePanel()
-    {
-        connectPanel.SetActive(false);
     }
 
     public void TogglePanel()
---
+++ Gathering/Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
