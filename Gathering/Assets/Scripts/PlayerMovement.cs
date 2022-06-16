e69808ff23bb94c13a6b30f8c825790ddfe86f21
PotatoPhantom <54708020+PotatoPhantom@users.noreply.github.com>
2022-06-16 00:46:04 -0700

    Update PlayerMovement.cs

--- Gathering/Assets/Scripts/PlayerMovement.cs
+++ Gathering/Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,8 @@ public class PlayerMovement : NetworkBehaviour
 
     [SerializeField] private Camera playerCamera;
     [SerializeField] private CharacterController controller;
-    [SerializeField] private float mouseSensitivity = 3f;
-    [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private float mouseSensitivity = 5f;
+    [SerializeField] private float moveSpeed = 3f;
     [SerializeField] private float gravity = -9.81f;
 
     private Vector3 velocity;
@@ -34,13 +34,32 @@ public class PlayerMovement : NetworkBehaviour
             mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
 
             Move();
+            Look();
         }
     }
 
     void Move()
     {
         Vector3 movementVector = transform.forward * keyInput.z + transform.right * keyInput.x;
+        controller.Move(movementVector * moveSpeed * Time.deltaTime);
 
-        transform.position += movementVector;
+        if (controller.isGrounded)
+        {
+            velocity.y = -3f;
+        } else
+        {
+            velocity.y -= gravity * -2f * Time.deltaTime;
+        }
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void Look()
+    {
+        xRotation -= mouseInput.y * mouseSensitivity;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.Rotate(0f, mouseInput.x, 0f);
+        playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
     }
 }
